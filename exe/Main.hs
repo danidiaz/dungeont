c198c333cc8119ef668c7ab063cb1c06180aa718
DDC <diaz_carrete@yahoo.com>
2017-07-07 19:33:12 +0200

    ...

--- exe/Main.hs
+++ exe/Main.hs
@@ -3,9 +3,44 @@
 module Main where
 
 import Brick
+import Streaming.Prelude
+import Control.Monad.Trans.State.Strict
+
+import Dungeon
+import Dungeon.Player
+import Dungeon.Player.Prelude
 
 ui :: Widget ()
 ui = str "Hello, world!"
 
+data S a m r = S !a !(Stream (Of a) m r)
+
 main :: IO ()
-main = simpleMain ui
+main = do
+    -- let app = App { ... }
+    -- :: s -> [Widget n],
+    -- :: s -> [CursorLocation n] -> Maybe (CursorLocation n)
+    -- :: s -> BrickEvent n e -> EventM n (Next s)
+    -- :: s -> EventM n s
+    -- :: s -> AttrMap
+    let app =
+            App 
+            { 
+              appDraw = undefined         
+            , appChooseCursor = undefined
+            , appHandleEvent = undefined
+            , appStartEvent = undefined
+            , appAttrMap = undefined  
+            }
+        initialDungeon = 
+            DungeonState 
+            {
+               player = Position 5 5
+            ,  treasures = [Position 1 2]
+            ,  traps = [Position 8 9]
+            }
+        initialState = 
+            S initialDungeon 
+              (flip evalStateT initialDungeon . runDungeonT $ approachTreasure 0)
+    _ <- defaultMain app initialState
+    return ()
