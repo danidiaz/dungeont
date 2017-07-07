daa3ce1422b2f203c9685de04e46f020434a7634
DDC <diaz_carrete@yahoo.com>
2017-07-07 20:56:20 +0200

    picture

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,10 +1,19 @@
+{-# language ScopedTypeVariables #-}
 {-# language NamedFieldPuns #-}
 
 module Main where
 
-import Brick
+import Data.Monoid
+import Data.Functor
+import Data.Functor.Identity
 import Streaming.Prelude
 import Control.Monad.Trans.State.Strict
+
+import Brick
+import qualified Brick.Main
+
+import Graphics.Vty.Image
+import Graphics.Vty.Attributes(defAttr)
 
 import Dungeon
 import Dungeon.Player
@@ -23,24 +32,26 @@ main = do
     -- :: s -> BrickEvent n e -> EventM n (Next s)
     -- :: s -> EventM n s
     -- :: s -> AttrMap
-    let app =
+    let app :: App (S DungeonState Identity ()) () () =
             App 
             { 
-              appDraw = undefined         
-            , appChooseCursor = undefined
-            , appHandleEvent = undefined
-            , appStartEvent = undefined
-            , appAttrMap = undefined  
+              appDraw = const $ [ raw $ vertCat $ [ Graphics.Vty.Image.string defAttr "XXXXoXXXX"
+                                                  , Graphics.Vty.Image.string defAttr "skdfjalsd"]
+                                ]         
+            , appChooseCursor = Brick.Main.neverShowCursor
+            , appHandleEvent = \s _ -> Brick.Main.continue s
+            , appStartEvent = return
+            , appAttrMap = const $ attrMap mempty []
             }
         initialDungeon = 
             DungeonState 
             {
-               player = Position 5 5
+               player = Position 6 5
             ,  treasures = [Position 1 2]
             ,  traps = [Position 8 9]
             }
         initialState = 
             S initialDungeon 
-              (flip evalStateT initialDungeon . runDungeonT $ approachTreasure 0)
+              (void $ flip evalStateT initialDungeon $ runDungeonT $ approachTreasure 0)
     _ <- defaultMain app initialState
     return ()
